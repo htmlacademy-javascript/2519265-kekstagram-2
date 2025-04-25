189b8eda4eb8957b15bb6bfb7e21c6b9d8d5828d
NatalyaDemidova <demidovanatalia691991@gmail.com>
2025-04-25 16:34:36 +0300

    fixing module7

--- js/createPictures.js
+++ js/createPictures.js
@@ -1,12 +1,13 @@
 const photoElementTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-export const createPhotosList = function (photos) {
+export const createPhotosList = (photos) => {
   const similarListFragment = document.createDocumentFragment();
 
   photos.forEach(({url, description, comments, likes}) => {
     const photoElement = photoElementTemplate.cloneNode(true);
-    photoElement.querySelector('.picture__img').src = url;
-    photoElement.querySelector('.picture__img').alt = description;
+    const img = photoElement.querySelector('.picture__img');
+    img.src = url;
+    img.alt = description;
     photoElement.querySelector('.picture__comments').textContent = comments.length;
     photoElement.querySelector('.picture__likes').textContent = likes;
     similarListFragment.appendChild(photoElement);
